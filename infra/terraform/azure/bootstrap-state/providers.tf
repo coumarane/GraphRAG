200aba0a479433c0118e8d54fcc0e35eb9c520ef
Coumarane <c.coumarane@gmail.com>
2026-08-16 00:48:08 +0200

    Add Azure Terraform storage stack

---
+++ infra/terraform/azure/bootstrap-state/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.12.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
+}
